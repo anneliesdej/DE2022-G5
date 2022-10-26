086049275874e37fc8d18507ff5a87861d8f3f38
RiesJads <113186998+RiesJads@users.noreply.github.com>
2022-10-26 10:20:34 +0200

    Update app.py

--- prediction-ui-embedded-gcs/app.py
+++ prediction-ui-embedded-gcs/app.py
@@ -1,43 +1,44 @@
-# importing Flask and other modules
 import json
 import os
 
 import pandas as pd
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 
-from diabetes_predictor import DiabetesPredictor
+from heartattack_predictor import HeartAttackPredictor
 
-# Flask constructor
+#Flask constructor
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-
-# A decorator used to tell the application
-# which URL is associated function
-@app.route('/checkdiabetes', methods=["GET", "POST"])
-def check_diabetes():
+# A decorator used to tell the application which URL is associated function
+@app.route('/predict_heartattack', methods=["GET", "POST"])
+def predict_heartattack():
     if request.method == "POST":
         prediction_input = [
             {
-                "ntp": int(request.form.get("ntp")),  # getting input with name = ntp in HTML form
-                "pgc": int(request.form.get("pgc")),  # getting input with name = pgc in HTML form
-                "dbp": int(request.form.get("dbp")),
-                "tsft": int(request.form.get("tsft")),
-                "si": int(request.form.get("si")),
-                "bmi": float(request.form.get("bmi")),
-                "dpf": float(request.form.get("dpf")),
+                "exng": int(request.form.get("exng")),
+                "caa": int(request.form.get("caa")),
+                "cp": int(request.form.get("cp")),
+                "fbs": int(request.form.get("fbs")),
+                "restecg": int(request.form.get("restecg")),
+                "slp": int(request.form.get("slp")),
+                "thall": int(request.form.get("thall")),
+                "trtbps": float(request.form.get("trtbps")),
+                "chol": float(request.form.get("chol")),
+                "thallachh": float(request.form.get("thallachh")),
+                "oldpeak": float(request.form.get("oldpeak")),
                 "age": int(request.form.get("age"))
             }
         ]
         print(prediction_input)
-        dp = DiabetesPredictor()
+        hap = HeartAttackPredictor()
         df = pd.read_json(json.dumps(prediction_input), orient='records')
-        status = dp.predict_single_record(df)
-        # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
+        status = hap.predict_single_record(df)
+        #return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
         return jsonify({'result': str(status[0])}), 200
 
     return render_template(
-        "user_form.html")  # this method is called of HTTP method is GET, e.g., when browsing the link
-
+        "user_form.html") #this method is called of HTTP method is GET
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
